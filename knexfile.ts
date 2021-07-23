0e70a6bdefe6d5efb88c1dde6c6d5e97f4d01ed5
Chiro Hiro <chiro8x@gmail.com>
2021-07-23 12:22:50 +0700

    fix warning

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,7 @@
 // Update with your config settings.
 import { Connector } from './src/framework/connector';
 import config from './src/helper/config';
+
 module.exports = {
   development: Connector.parseURL(config.mariadbConnectUrl),
 };
