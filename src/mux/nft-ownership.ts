80dafdd099caae2aa848a96315f34f259a3a7c7b
Chiro Hiro <chiro8x@gmail.com>
2021-12-03 15:44:23 +0700

    Add tokenSymbol filter for nftOwnership

--- src/mux/nft-ownership.ts
+++ src/mux/nft-ownership.ts
@@ -4,22 +4,36 @@ import ModelNftOwnership from '../model/model-nft-ownership';
 
 Mux.get(
   '/api/v1/nftOwnership',
-  new Validator({
-    name: 'owner',
-    type: 'string',
-    validator: (e): boolean => utils.isAddress(e),
-    location: 'query',
-  }).merge(Pagination.getPaginationValidator(0, 500)),
+  new Validator(
+    {
+      name: 'owner',
+      type: 'string',
+      validator: (e): boolean => utils.isAddress(e),
+      location: 'query',
+    },
+    {
+      name: 'tokenSymbol',
+      type: 'string',
+      validator: (e): boolean => /[A-Z]{3,32}/.test(e),
+      location: 'query',
+    },
+  ).merge(Pagination.getPaginationValidator(0, 500)),
   async (req: IRequestData) => {
     const imNftOwnership = new ModelNftOwnership();
     const conditions: any[] = [];
     const {
-      query: { owner },
+      query: { owner, tokenSymbol },
     } = req;
     if (typeof owner === 'string') {
       conditions.push({
         field: 'owner',
         value: owner,
+      });
+    }
+    if (typeof tokenSymbol === 'string') {
+      conditions.push({
+        field: 't.symbol',
+        value: tokenSymbol,
       });
     }
     return imNftOwnership.getNftList(req.query, conditions);
